7e7505b8d9abe0acf6d59d10b7b00d173e0addcc
Deonta-Dubz <130009900+Deonta-Dubz@users.noreply.github.com>
2024-04-22 10:04:53 -0400

    Update jsonUtilities.py documentation

--- FinalProject/jsonUtilitiesPackage/jsonUtilities.py
+++ FinalProject/jsonUtilitiesPackage/jsonUtilities.py
@@ -1,6 +1,6 @@
 #jsonUtilities.py
-# Name: Brianna Jarrell, Leonie Troeger, Thomas Gilligan
-# email: jarrelbc@mail.uc.edu, troegele@mail.uc.edu, gilligtp@mail.uc.edu
+# Name: Brianna Jarrell, Leonie Troeger, Thomas Gilligan, Deonta Williams
+# email: jarrelbc@mail.uc.edu, troegele@mail.uc.edu, gilligtp@mail.uc.edu, willi6d3@mail.uc.edu
 # Assignment Number: Final Project
 # Due Date: 4-23-24
 # Course/Section:IS 4010-002
@@ -8,6 +8,12 @@
 # Brief Description of the assignment: Extract Tricia McMillian word rows
 # Citations:
 # Anything else that's relevant:
+
+
+"""
+This py file imports json then opens the json file to print the data for desired 
+name.
+"""
 
 import json
 
