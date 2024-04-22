4b7d19fe10a0c39bb3d39dcea6ea3da30b4d467c
Deonta-Dubz <130009900+Deonta-Dubz@users.noreply.github.com>
2024-04-22 09:46:45 -0400

    Update image.py and documentation

--- FinalProject/dataPackage/image.py
+++ FinalProject/dataPackage/image.py
@@ -1,6 +1,6 @@
 #image.py 
-# Name: Brianna Jarrell, Leonie Troeger, Thomas Gilligan
-# email: jarrelbc@mail.uc.edu, troegele@mail.uc.edu, gilligtp@mail.uc.edu
+# Name: Brianna Jarrell, Leonie Troeger, Thomas Gilligan, Deonta Williams
+# email: jarrelbc@mail.uc.edu, troegele@mail.uc.edu, gilligtp@mail.uc.edu, willi6d3@mail.uc.edu
 # Assignment Number: Final Project
 # Due Date: 4-23-24
 # Course/Section:IS 4010-002
@@ -8,6 +8,11 @@
 # Brief Description of the assignment: To show and store group photo
 # Citations:
 # Anything else that's relevant:
+
+
+"""
+this python file imports the PIL library and uses open function to read our jpg file
+"""
 
 # Importing Image class from PIL module
 from PIL import Image
