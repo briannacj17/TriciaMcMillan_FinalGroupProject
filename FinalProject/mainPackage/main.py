dba39e757526cd642a203011c2eb58fe63a147ae
Deonta-Dubz <130009900+Deonta-Dubz@users.noreply.github.com>
2024-04-22 10:10:51 -0400

    Update main.py documentation

--- FinalProject/mainPackage/main.py
+++ FinalProject/mainPackage/main.py
@@ -1,6 +1,6 @@
 #main.py
-# Name: Brianna Jarrell, Leonie Troeger, Thomas Gilligan
-# email: jarrelbc@mail.uc.edu, troegele@mail.uc.edu, gilligtp@mail.uc.edu
+# Name: Brianna Jarrell, Leonie Troeger, Thomas Gilligan, Deonta Williams
+# email: jarrelbc@mail.uc.edu, troegele@mail.uc.edu, gilligtp@mail.uc.edu, willi6d3@mail.uc.edu
 # Assignment Number: Final Project
 # Due Date: 4-23-24
 # Course/Section:IS 4010-002
@@ -8,6 +8,15 @@
 # Brief Description of the assignment: Decrypting complex code
 # Citations:
 # Anything else that's relevant:
+
+
+"""
+ENTRY POINT
+This main py file imports all the libraries and packages with their 
+functions we made or used to run our app. The app runs our encryption, 
+prints the decyption numbers and decrypted group location
+and finally prints out the group picture
+"""
 
 import json
 from dataUtilitiesPackage.decryption import *
